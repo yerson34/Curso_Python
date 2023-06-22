b98ebf18bfe6ef9870c1f1bc13849986cd5c3372
yerson34 <132001298+yerson34@users.noreply.github.com>
2023-06-22 12:34:55 -0500

    Add files via upload

---
+++ BUCLES.py
@@ -0,0 +1,8 @@
+colores = ["azul", "verde", "rojo", "amarillo", "naranja"]
+
+for color in colores:
+    if color == "rojo":
+        print("¡Color encontrado!")
+        break
+    else:
+        print(color)
